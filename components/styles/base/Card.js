a192517d494f523e4c9918c7a664507c45d663d8
ErnieBuyco <buycoernie@gmail.com>
2020-04-30 14:40:17 +0800

    added_media_querys

--- components/styles/base/Card.js
+++ components/styles/base/Card.js
@@ -40,11 +40,26 @@ const Card = styled.div`
                   &-3 {
                           background-image: linear-gradient(to right bottom, ${props => props.theme.color_})
                   }
+
+                  @media(max-width: ${props => props.theme.screens.tab_port}), only screen and (hover:none){
+                        transform: rotateY(0);
+                        clip-path: polygon(0 15%, 100% 0, 100% 100%, 0% 100%);
+                  }
+          }
+
+          @media(max-width: ${props => props.theme.screens.tab_port}), only screen and (hover:none){
+                height: auto;
+                position: relative;
+                box-shadow: none;
           }
       }
 
       &:hover &__side--front {
             transform: rotateY(-180deg);
+
+            @media(max-width: ${props => props.theme.screens.tab_port}), only screen and (hover:none){
+                transform: rotateY(0);
+            }
       }
 
       &:hover &__side--back {
@@ -88,6 +103,101 @@ const Card = styled.div`
             width: 75%;
       }
 
+      &__heading-span {
+            padding: 1rem 1.5rem;
+            -webkit-box-decoration-break: clone;
+            box-decoration-break: clone;
+
+            &--1 {
+                  background-image: linear-gradient(to right bottom,
+                    rgba(255,185,0,0.85), rgba(255,119,48,0.85));
+            }
+
+            &--2 {
+                  background-image: linear-gradient(to right bottom,
+                    rgba(126,213,111,0.85), rgba(40,180,133,0.85));
+                }
+            &--3 {
+                background-image: linear-gradient(to right bottom,
+                  rgba(41,152,255,0.85), rgba(86,67,250,0.85)
+                );
+            }
+      }
+
+      &__details {
+              padding: 3rem;
+
+              ul {
+                      list-style: none;
+                      width: 80%;
+                      margin: 0 auto;
+
+
+                      li {
+                            text-align: center;
+                            font-size: 1.5rem;
+                            padding: 1rem;
+
+                            &:not(:last-child){
+                                border-bottom: 1px solid ${props => props.theme.color_grey_light_2};
+                            }
+                      }
+              }
+              @media(max-width: ${props => props.theme.screens.tab_port}), only screen and (hover:none){
+                    padding: 1rem 3rem;
+              }
+      }
+
+      &__cta {
+              position: absolute;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+              width: 90%;
+              text-align: center;
+
+              @media(max-width: ${props => props.theme.screens.tab_port}), only screen and (hover:none){
+                  position: relative;
+                  top: 0%;
+                  left: 0;
+                  transform: translate(0);
+                  width: 100%;
+                  padding: 7rem 4rem 4rem 4rem;
+              }
+      }
+
+      &__price-box{
+            text-align:center;
+            color: ${props => props.theme.base};
+
+            @media(max-width: ${props => props.theme.screens.tab_port}), only screen and (hover:none){
+                margin-bottom: 3rem;
+            }
+      }
+
+      &__price-only{
+          font-size: 1.4rem;
+          text-transform: uppercase;
+      }
+
+      &__price-value{
+            font-size: 6rem;
+            font-weight: 100;
+
+            @media(max-width: ${props => props.theme.screens.tab_port}), only screen and (hover:none){
+                font-size: 4rem;
+            }
+      }
+
+      @media(max-width: ${props => props.theme.screens.tab_port}), only screen and (hover:none){
+
+            height: auto;
+            border-radius: 3px;
+            background-color: ${props => props.theme.base};
+            box-shadow: 0 1.5rem 4rem rgba(0,0,0,0.15);
+
+
+      }
 
      }
 
