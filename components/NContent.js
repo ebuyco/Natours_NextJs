ee1ab52400143bcfd9b86151631081b8eb1910e6
ErnieBuyco <buycoernie@gmail.com>
2020-05-01 01:27:58 +0800

    refactor

--- components/NContent.js
+++ components/NContent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 const NContent = (props) => {
@@ -8,75 +8,11 @@ const NContent = (props) => {
       attachedClasses =  [props.Open ? 'Open' : 'Open'];
   }
 
+    
 
   return(
       <>
-            <div className="navigation">
-                    <input type="checkbox" className="navigation__checkbox"/>
 
-                    <label htmlFor="navi-toggle"
-                     className="navigation__button"
-                     onClick={props.drawerToggleClicked}
-                    >
-                            <span
-                            className="navigation__icon"
-                            >&nbsp;</span>
-                    </label>
-
-                    <div className="navigation__background">&nbsp;</div>
-
-                    <nav
-
-                    className={`navigation__nav ${attachedClasses.join('')}`}
-                    >
-                         <label htmlFor="navi-toggle"
-                         className="navigation__button">
-
-                    </label>
-                          <ul className="navigation__list">
-                                    <li className="navigation__item">
-                                        <a href="#about" className="navigation__link">
-                                            <span>
-                                              01
-                                            </span>
-                                            About Natous
-                                        </a>
-                                    </li>
-                                    <li className="navigation__item">
-                                        <a href="#" className="navigation__link">
-                                                <span>
-                                                  03
-                                                </span>
-                                                Popular tours
-                                            </a>
-                                    </li>
-                                    <li className="navigation__item">
-                                        <a href="#" className="navigation__link">
-                                                <span>
-                                                  02
-                                                </span>
-                                                Your Benefits
-                                            </a>
-                                    </li>
-                                    <li className="navigation__item">
-                                        <a href="#" className="navigation__link">
-                                                <span>
-                                                  04
-                                                </span>
-                                                Stories
-                                            </a>
-                                    </li>
-                                    <li className="navigation__item">
-                                        <a href="#" className="navigation__link">
-                                                <span>
-                                                  05
-                                                </span>
-                                               Book Now
-                                            </a>
-                                    </li>
-                          </ul>
-                    </nav>
-              </div>
       </>
   )
 };
--- components/Navigation.js
+++ components/Navigation.js
@@ -1,27 +1,82 @@
 import React , {useState, useRef} from 'react';
 import NavStyle from './styles/layout/NavStyle';
-import NContent from './NContent';
+
 
 const Navigation = () => {
 
-  const [sideDrawerIsVisible, setSideDrawerIsVisible] = useState(false);
-
-  const sideDrawerClosedHandler = () => {
-    setSideDrawerIsVisible(false);
-  };
-
-  const sideDrawerToggleHandler = () => {
-    setSideDrawerIsVisible(!sideDrawerIsVisible);
-    console.log('visible');
-  };
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
       return(
           <NavStyle>
-            <NContent
-          drawerToggleClicked={sideDrawerToggleHandler}
-          open={sideDrawerIsVisible}
-          closed={sideDrawerClosedHandler}
-              />
+              <div className="navigation">
+                    {/* <a href="#" className={`name__tagger__${isOpen ? 'm-menu--active' : 'menu'}`} onClick={toggle}>Tet</a> */}
+                  <input type="checkbox" className="navigation__checkbox"/>
+
+                    <label htmlFor="navi-toggle"
+                    //  className="navigation__button"
+                    // className={`navigation__button--${attachedClasses.join('')}`}
+                     onClick={toggle}
+                    >
+                            <span
+                            className="navigation__icon"
+                            >&nbsp;</span>
+                    </label>
+
+                    <div className="navigation__background">&nbsp;</div>
+
+                    <nav
+                      className={`navigation__nav ${isOpen ? 'Open' : 'Close'}`}
+                    // className={`navigation__nav ${attachedClasses.join('')}`}
+                    >
+                         <label htmlFor="navi-toggle"
+                         className="navigation__button">
+
+                    </label>
+                          <ul className="navigation__list">
+                                    <li className="navigation__item">
+                                        <a href="#about" className="navigation__link">
+                                            <span>
+                                              01
+                                            </span>
+                                            About Natous
+                                        </a>
+                                    </li>
+                                    <li className="navigation__item">
+                                        <a href="#" className="navigation__link">
+                                                <span>
+                                                  03
+                                                </span>
+                                                Popular tours
+                                            </a>
+                                    </li>
+                                    <li className="navigation__item">
+                                        <a href="#" className="navigation__link">
+                                                <span>
+                                                  02
+                                                </span>
+                                                Your Benefits
+                                            </a>
+                                    </li>
+                                    <li className="navigation__item">
+                                        <a href="#" className="navigation__link">
+                                                <span>
+                                                  04
+                                                </span>
+                                                Stories
+                                            </a>
+                                    </li>
+                                    <li className="navigation__item">
+                                        <a href="#" className="navigation__link">
+                                                <span>
+                                                  05
+                                                </span>
+                                               Book Now
+                                            </a>
+                                    </li>
+                          </ul>
+                    </nav>
+              </div>
           </NavStyle>
       )
 }
