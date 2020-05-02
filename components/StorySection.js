67074a0a8f70637676c4d2a7ee5c5c5c36f60e41
ErnieBuyco <buycoernie@gmail.com>
2020-05-02 22:05:38 +0800

    story_section_content_added

--- components/StorySection.js
+++ components/StorySection.js
@@ -10,6 +10,62 @@ const StorySection = () => (
                                     <source src="../static/video.webm" type="video/webm"/>
                             </video>
                     </div>
+
+                    <div className="u-center-text u-margin-bottom-big">
+                                <h2 className="heading-secondary">
+                                      We make people genuinely happy
+                                </h2>
+                    </div>
+
+                    <div className="row">
+                               <div className="story">
+                                        <figure className="story__shape">
+                                                <img src="../static/nat-8.jpg"
+                                                 alt="Person on a tour"
+                                                 className="story__img"/>
+                                                 <figcaption className="story__caption">
+                                                         Mary Smith
+                                                 </figcaption>
+                                        </figure>
+                                        <div className="story__text">
+                                                <h3 className="heading-tertiary u-margin-bottom-small">
+                                                          I had the best week ever with my family
+                                                </h3>
+                                                <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur
+                                                ducimus quam nisi exercitationem omnis earum qui. Aperiam, ipsum sapiente aspernatur libero
+                                                repellat quis consequatur ducimus quam nisi exercitationem omnis earum qui.
+                                                </p>
+                                        </div>
+                               </div>
+                    </div>
+
+                    <div className="row">
+                               <div className="story">
+                                        <figure className="story__shape">
+                                                <img src="../static/nat-9.jpg"
+                                                 alt="Person on a tour"
+                                                 className="story__img"/>
+                                                 <figcaption className="story__caption">
+                                                         Mary Smith
+                                                 </figcaption>
+                                        </figure>
+                                        <div className="story__text">
+                                                <h3 className="heading-tertiary u-margin-bottom-small">
+                                                          I had the best week ever with my family
+                                                </h3>
+                                                <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur
+                                                ducimus quam nisi exercitationem omnis earum qui. Aperiam, ipsum sapiente aspernatur libero
+                                                repellat quis consequatur ducimus quam nisi exercitationem omnis earum qui.
+                                                </p>
+                                        </div>
+                               </div>
+                    </div>
+
+                    <div className="u-center-text u-margin-top-huge">
+                            <a href="#" className="btn-next">Read all stories &rarr;</a>
+                    </div>
            </section>
       </>
 );
