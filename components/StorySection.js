caeb2c7b0991716995b2525b22a8b3b6254754e2
ErnieBuyco <buycoernie@gmail.com>
2020-05-02 16:19:06 +0800

    video_assets

--- components/StorySection.js
+++ components/StorySection.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import VMp4 from '../public/static/video.mp4';
-import VWebm from '../public/static/video.webm';
+
 
 const StorySection = () => (
       <>
            <section className="section-stories">
                     <div className="bg-video">
                             <video autoPlay muted loop className="bg-video__content">
-                                    <source src={VMp4} type="video/mp4"/>
-                                    <source src={VWebm} type="video/webm"/>
+                                    <source src="../static/video.mp4" type="video/mp4"/>
+                                    <source src="../static/video.webm" type="video/webm"/>
                             </video>
                     </div>
            </section>
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
     'postcss-easy-import',
-  'autoprefixer'
+    'autoprefixer'
   ]
 }
